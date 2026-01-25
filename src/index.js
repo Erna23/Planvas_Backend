02824e95891f5c68807b1714ec9a865fdf77ec66
Aqoom01 <rlagusgh789@daum.net>
2026-01-25 18:34:47 +0900

    feat: 기능에 맞춰 db 수정

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import { registerUserRoutes } from "./controllers/user.controller.js";
 import { registerCalendarRoutes } from "./controllers/calendar.controller.js";
 import { registerGoalRoutes } from "./controllers/goals.controller.js";
+import { registerReportRoutes } from "./controllers/report.controller.js";
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ app.use(express.json());
 registerUserRoutes(app);
 registerCalendarRoutes(app);
 registerGoalRoutes(app);
+registerReportRoutes(app);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
