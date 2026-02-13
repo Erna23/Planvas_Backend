06f207949e69c4b6fba9fcad92e18c8d1d77065e
Aqoom01 <rlagusgh789@daum.net>
2026-02-14 03:26:17 +0900

    fix: 리포트 생성 수정

--- src/dtos/report.dto.js
+++ src/dtos/report.dto.js
@@ -17,12 +17,12 @@ export function toGoalReportDto(goal, growth, rest, summary) {
             }
         },
         summary: {
-            type: report.type,
-            title: report.title,
-            subTitle: report.subTitle
+            type: summary.type,
+            title: summary.msg?.title ?? "",
+            subTitle: summary.msg?.subTitle ?? ""
         },
-        cta:{
-            primary:{
+        cta: {
+            primary: {
                 type: "RECOMMEND",
                 focus: summary.recommend.focus,
                 label: summary.recommend.text
@@ -40,50 +40,50 @@ const Recommend = {
     GROWTH: "GROWTH",
     REST: "REST",
     NONE: "NONE",
-  };
-  
-  const SummaryType = {
+};
+
+const SummaryType = {
     ALL_LACK: "ALL_LACK",
     GROWTH_LACK: "GROWTH_LACK",
     REST_LACK: "REST_LACK",
     PERFECT: "PERFECT",
-  };
-  
-  const SUMMARY_MESSAGE = {
+};
+
+const SUMMARY_MESSAGE = {
     [SummaryType.ALL_LACK]: {
-      title: "이번 주는 전체적으로 활동이 부족해요",
-      subTitle: "성장/휴식 활동을 조금씩 추가해보는 건 어때요?",
+        title: "이번 주는 전체적으로 활동이 부족해요",
+        subTitle: "성장/휴식 활동을 조금씩 추가해보는 건 어때요?",
     },
     [SummaryType.GROWTH_LACK]: {
-      title: "성장 활동이 목표에 비해 부족해요",
-      subTitle: "간단한 성장 활동부터 하나 추가해보세요!",
+        title: "성장 활동이 목표에 비해 부족해요",
+        subTitle: "간단한 성장 활동부터 하나 추가해보세요!",
     },
     [SummaryType.REST_LACK]: {
-      title: "휴식 활동이 목표에 비해 부족해요",
-      subTitle: "컨디션을 위해 휴식도 일정에 넣어주세요 🙂",
+        title: "휴식 활동이 목표에 비해 부족해요",
+        subTitle: "컨디션을 위해 휴식도 일정에 넣어주세요 🙂",
     },
     [SummaryType.PERFECT]: {
-      title: "목표에 아주 잘 맞게 진행 중이에요!",
-      subTitle: "좋아요! 지금 페이스를 유지해보세요 🔥",
+        title: "목표에 아주 잘 맞게 진행 중이에요!",
+        subTitle: "좋아요! 지금 페이스를 유지해보세요 🔥",
     },
-  };
+};
 
 export async function getSummaryDto(goal, actualGrowth, actualRest) {
     let recommend;
     let type;
-    if(actualGrowth + actualRest < 50) {
+    if (actualGrowth + actualRest < 50) {
         type = "ALL_LACK"
         recommend = {
-            focus : Recommend.ALL,
+            focus: Recommend.ALL,
             text: "새로운 활동 탐색하기"
         }
-    } else if(goal.growth > goal.rest && actualGrowth / goal.growth * 100 < 40) {
+    } else if (goal.growth > goal.rest && actualGrowth / goal.growth * 100 < 40) {
         type = "GROWTH_LACK"
         recommend = {
             focus: Recommend.GROWTH,
             text: "새로운 성장 활동 탐색하기"
         }
-    } else if(goal.rest > goal.growth && actualRest / goal.rest * 100 < 40) {
+    } else if (goal.rest > goal.growth && actualRest / goal.rest * 100 < 40) {
         type = "REST_LACK"
         recommend = {
             focus: Recommend.REST,
